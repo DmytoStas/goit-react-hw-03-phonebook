6bef0a551e0ecce84a1f0909ce5cbff8b29dc87f
Dmytro Stas <dima7stas777@gmail.com>
2023-07-11 19:57:40 -0400

    hw-3 task-1 done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,16 +7,31 @@ import ContactsList from './ContactsList';
 import Filter from './Filter';
 import PhonebookForm from './PhonebookForm/PhonebookForm';
 
+const LS_KEY = 'contacts';
+
 export default class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem(LS_KEY);
+    const parseContacts = JSON.parse(contacts);
+
+    if (parseContacts) {
+      this.setState({ contacts: parseContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const newContacts = this.state.contacts;
+    const prevContacts = prevState.contacts;
+
+    if (newContacts !== prevContacts) {
+      localStorage.setItem(LS_KEY, JSON.stringify(newContacts));
+    }
+  }
 
   handleSubmit = values => {
     const { name, number } = values;
